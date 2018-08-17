a2a5102d63127dd4e4d1ff28535e52a5f77bcd78
ZIKAI ZHAO <u5655826@anu.edu.au>
2018-08-17 13:37:40 +1000

    Apply css module

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './NavBar.css';
+import classes from './NavBar.css';
 const navBar = (props) => (
-  <div className="NavBar">
-    <p className="NavBarItem">The program</p>
-    <p className="NavBarItem">Membership</p>
-    <p className="NavBarItem">Library</p>
-    <p className="NavBarItem">About us</p>
-    <p className="NavBarItem">Contact us</p>
+  <div className={classes.NavBar}>
+    <p className={classes.NavBarItem}>The program</p>
+    <p className={classes.NavBarItem}>Membership</p>
+    <p className={classes.NavBarItem}>Library</p>
+    <p className={classes.NavBarItem}>About us</p>
+    <p className={classes.NavBarItem}>Contact us</p>
   </div>
 )
 
